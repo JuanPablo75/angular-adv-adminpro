259c8643c28c2562225c588952421a806f717829
Juan Pablo <alujuanpablo.it@gmail.com>
2024-02-15 09:26:28 +0100

    URL3

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 //TODO: Cambiar el Url de los servicios en PRO
 
 export const environment = {
-    production: false,
+    production: true,
     base_url:  'https://adminpro-backend-i81q.onrender.com/api',
 }
